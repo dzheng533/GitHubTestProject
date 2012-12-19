da0f22ed16048e007f48796e6d8e8b33f2448bfa
Vincent <Vincent@VincentThinkpad>
2012-12-19 16:38:32 +0800

    fff

--- src/com/cyberdis/HelloGitHub.java
+++ src/com/cyberdis/HelloGitHub.java
@@ -8,6 +8,8 @@ public class HelloGitHub {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("Hello GitHub.");
+		//Second commit
+		System.out.print("Get from GitHub.Commit by Zheng");
 	}
 
 }
