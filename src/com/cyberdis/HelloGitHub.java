ed60073b91877403a1f194fe76675760ffec83c3
ZhengD <zhengd@wintaoglobal.com>
2012-12-19 15:32:36 +0800

    Initialize commit

---
+++ src/com/cyberdis/HelloGitHub.java
@@ -0,0 +1,13 @@
+package com.cyberdis;
+
+public class HelloGitHub {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello GitHub.");
+	}
+
+}
